8a003896b93b1da046dc92535d80068f2d068c72
devika-bej <devika.bej@research.iiit.ac.in>
2024-03-03 04:04:12 +0530

    can make polygons of different sizes

---
+++ fragment_shader.fs
@@ -0,0 +1,7 @@
+#version 330 core
+out vec4 FragColor;
+
+void main() {
+    FragColor = vec4(16, 16, 16, 1);
+    // setting a single default color
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include "glad.h"
+#include "shader.h"
 #include <GLFW/glfw3.h>
+#include <math.h>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -16,20 +18,80 @@ void processInput(GLFWwindow *window)
 const unsigned int WIDTH = 600;
 const unsigned int HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "   FragColor = vec4(0.738, 0.707, 0.832, 1.0f);\n"
-                                   "}\n\0";
-bool pyramid()
+bool pyramid(GLFWwindow *window)
 {
+    Shader myShader("./vector_shader.vs", "./fragment_shader.fs");
+
+    int n = 9;
+    // setting up vertices
+    float vertices[3 * n];
+    for (int i = 0; i < n; ++i)
+    {
+        float x = cosf(2 * M_PI * i / n);
+        float y = sinf(2 * M_PI * i / n);
+        vertices[3 * i] = x;
+        vertices[3 * i + 1] = y;
+        vertices[3 * i + 2] = 0.0f;
+    }
+    // setting up indices list for each triangle, start with 0, and next 2 indices for next triangle
+    unsigned int indices[3 * (n - 2)];
+    for (int i = 0; i < n - 2; ++i)
+    {
+        indices[3 * i] = 0;
+        indices[3 * i + 1] = i + 1;
+        indices[3 * i + 2] = i + 2;
+    }
+
+    unsigned int VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    // index = 0, size = 3, float type, dont normalize, since each attribute is x,y,z therefore stride = 3*size of float, type of draw = static
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    while (not glfwWindowShouldClose(window))
+    {
+        processInput(window);
+
+        glClearColor(0.5569, 0.569, 0.56, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        myShader.use();
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 3 * n, GL_UNSIGNED_INT, 0);
+        // type of element is triangle, 3*n elements in vertices, indices is of uint type, 0 is pointer to indices
+
+        glfwSwapBuffers(window); // rendering window
+        glfwPollEvents();        // updating window
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glfwTerminate();
+    return true;
+}
+
+int main(int argc, char **argv)
+{
+    // set up for opengl 3
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                 // major version - 3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // minor version - 3
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use core profile
+
     // window creation
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Scene 1", nullptr, nullptr);
     if (not window)
@@ -48,101 +110,7 @@ bool pyramid()
         return false;
     }
 
-    int success;
-    char infoLog[512];
-
-    // vertex shader set up
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
-    // vertex shader error check
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (not success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, nullptr, infoLog);
-        std::cout << "Vertex shader not compiled\n";
-    }
-
-    // fragment shader set up
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
-    // fragment shader error check
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (not success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, nullptr, infoLog);
-        std::cout << "Fragment shader not compiled\n";
-    }
-
-    // linking shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // linking error check
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success)
-    {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "Shaders not linked\n";
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    float vertices[] = {0.0, 1.0, 0.0, 1 / 1.732, -0.5, 0.0, -1 / 1.732, -0.5, 0.0};
-    unsigned int indices[] = {0, 1, 2};
-
-    unsigned int VBO, VAO, EBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
-    glBindVertexArray(VAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
-    while (not glfwWindowShouldClose(window))
-    {
-        processInput(window);
-
-        glClearColor(0.5569, 0.569, 0.56, 1.0);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
-
-        glfwSwapBuffers(window); // rendering window
-        glfwPollEvents();        // updating window
-    }
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
-    glDeleteProgram(shaderProgram);
-
-    glfwTerminate();
-    return true;
-}
-
-int main(int argc, char **argv)
-{
-    // set up for opengl 3
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                 // major version - 3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);                 // minor version - 3
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // use core profile
-
-    if (not pyramid())
+    if (not pyramid(window))
     {
         return -1;
     }
